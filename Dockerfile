4820b1845d11ad4e0ea214d7764c108ed9a122d0
dewdroid <thossapoldew@gmail.com>
2025-01-15 00:17:38 +0700

    fix

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM python:3.12-slim
 
 WORKDIR /app
 
-COPY requirements.txt /app
+COPY requirements.txt .
 
-RUN pip install --no-cache-dir --upgrade -r requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
 
-COPY ./app /app
+COPY . .
 
 CMD ["fastapi", "run"]
--- main.py
+++ main.py
@@ -1,27 +1,7 @@
-from contextlib import asynccontextmanager
-from functools import lru_cache
-
-import google.generativeai as genai
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.config import Settings
-from app.apis.api_v1 import api_router_v1
-
-
-@lru_cache
-def get_settings():
-    return Settings()
-
-
-@asynccontextmanager
-async def lifespan(app: FastAPI):
-    settings = get_settings()
-    genai.configure(api_key=settings.gemini_api_key)
-    yield
-
-
-app = FastAPI(lifespan=lifespan)
+app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
@@ -31,4 +11,6 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
-app.include_router(api_router_v1, prefix='/api/v1')
+@app.get("/")
+def read_root():
+    return {"message": "ok"}
